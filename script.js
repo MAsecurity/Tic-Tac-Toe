e944679a0bf6b1da62fc699c49c3f42f22058bb3
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2023-09-21 14:23:44 +0100

    Make AI more smarter

--- script.js
+++ script.js
@@ -84,53 +84,33 @@ const gameBoardFunc = (player1, player2) => {
     // let cardBoard = [0,1,2,
     //                  3,4,5,
     //                  6,7,8]
-
     //Check for Horizontal wins and its not equal to empty ''
     if (allDivs[0].textContent == allDivs[1].textContent && allDivs[0].textContent == allDivs[2].textContent && allDivs[0].textContent != ''){
       return (allDivs[0].textContent == "X") ? "X" : "O";
-
     }else if(allDivs[3].textContent == allDivs[4].textContent &&  allDivs[3].textContent == allDivs[5].textContent && allDivs[3].textContent != '') {
         return (allDivs[3].textContent == "X") ? "X" : "O";
-
-
     }else if (allDivs[6].textContent == allDivs[7].textContent && allDivs[6].textContent == allDivs[8].textContent && allDivs[6].textContent != ''){
         return (allDivs[6].textContent == "X") ? "X" : "O";
- 
-
     }
     //Check for vertical wins
     if (allDivs[0].textContent == allDivs[3].textContent && allDivs[0].textContent == allDivs[6].textContent && allDivs[0].textContent != '') {
-      return (allDivs[0].textContent == "X") ? "X" : "O";
-
-      
+      return (allDivs[0].textContent == "X") ? "X" : "O"; 
     }else if(allDivs[1].textContent == allDivs[4].textContent && allDivs[1].textContent == allDivs[7].textContent && allDivs[1].textContent != '') {
       return (allDivs[1].textContent == "X") ? "X" : "O";
-   
-
     }else if (allDivs[2].textContent == allDivs[5].textContent && allDivs[2].textContent == allDivs[8].textContent && allDivs[2].textContent != '') {
       return (allDivs[2].textContent == "X") ? "X" : "O";
-
     }
-    
     //Check for Diagonal wins 
     if (allDivs[0].textContent == allDivs[4].textContent && allDivs[0].textContent == allDivs[8].textContent && allDivs[0].textContent != ''){
       return (allDivs[0].textContent == "X") ? "X" : "O";
-
     }else if(allDivs[2].textContent == allDivs[4].textContent && allDivs[2].textContent == allDivs[6].textContent && allDivs[2].textContent != '') {
       return (allDivs[2].textContent == "X") ? "X" : "O";
-
     }
-    
     if (allDivs[0].textContent != '' && allDivs[1].textContent != '' && allDivs[2].textContent != '' && allDivs[3].textContent != '' && allDivs[4].textContent != '' && allDivs[5].textContent != '' && allDivs[6].textContent != '' && allDivs[7].textContent != '' && allDivs[8].textContent != '') {
       //Game is a draw and no empty divs left
       return "Draw";
-
-
     }
-  
-
   }
-
   const _clear = () => {
     //Should reset all values when the reset button is clicked
     playAgain.addEventListener("click", () => {
@@ -188,6 +168,8 @@ const gameBoardFunc = (player1, player2) => {
   const _makeAiMoves = () => {
     let randomValue;
     let validatedNumber = false;
+    let value = _findBestAiMove();
+    console.log(value);
     while(validatedNumber == false){
       //Generate random values ranging from 0-8
       randomNumber = Math.floor(Math.random()*9);
@@ -198,8 +180,20 @@ const gameBoardFunc = (player1, player2) => {
         validatedNumber = true;
 
       }
-    }  
-    if(allDivs[randomValue].textContent == ''){
+    }
+    //Find best Ai move
+    if(value != "None"){
+      //Add O
+      allDivs[value].textContent = "O";
+      allDivs[value].classList.add("player2-css");
+      playerStatus.textContent = "Player 1 Turn (X):"
+      //Call switchplayer function to change current player
+      _switchPlayer();
+      //Call checkWinner function
+      let result = _checkWinner();
+      //Declare winner
+      _declareWinner(result);
+    }else if (value == "None"){
       //Add O
       allDivs[randomValue].textContent = "O";
       allDivs[randomValue].classList.add("player2-css");
@@ -209,10 +203,71 @@ const gameBoardFunc = (player1, player2) => {
       //Call checkWinner function
       let result = _checkWinner();
       //Declare winner
-      _declareWinner(result)
+      _declareWinner(result);
+    }
 
+  }
+  
+    
+  const _findBestAiMove = () => {
+    let currentState = [];
+    let findEmptyIndex = [];
+    let enemyMark = "X";
+    let bestAiMove;
+    allDivs.forEach(divItem => {
+      currentState.push(divItem.textContent)
+    })
+    for (let i=0; i<currentState.length; i++){
+      if(currentState[i] == ''){
+        findEmptyIndex.push(i);
       }
-    } 
+      }
+
+    for(let i=0; i<findEmptyIndex.length; i++){
+      let currentIndex = findEmptyIndex[i];
+      let testState = currentState;
+      testState.splice(currentIndex,1,enemyMark);
+      let result = _testBoard(testState,enemyMark);
+      if(result == 10 ){
+        bestAiMove = findEmptyIndex[i];
+        return bestAiMove;
+        break;   
+      }
+      testState.splice(currentIndex,1,'');
+    }
+    if (bestAiMove == null){
+      return "None";
+    }
+    }
+
+  const _testBoard = (board, enemyMark) => {
+    // let cardBoard = [0,1,2,
+    //                  3,4,5,
+    //                  6,7,8]
+    //Check for Horizontal wins
+    if(board[0] == board[1] && board[0] == board[2] && board[0] == enemyMark){
+      return 10;
+    }else if (board[3] == board[4] && board[3] == board[5] && board[3] == enemyMark){
+      return 10;
+    }else if (board[6] == board[7] && board[6] == board[8] && board[6] == enemyMark){
+      return 10;
+    }else if(board[0] == board[3] && board[0] == board[6] && board[0] == enemyMark){
+      //Check for vertical wins
+      return 10;
+    }else if(board[1] == board[4] && board[1] == board[7] && board[1] == enemyMark){
+      return 10;
+    }else if (board[2] == board[5] && board[2] == board[8] && board[2] == enemyMark){
+      return 10;
+    }else if(board[0] == board[4] && board[0] == board[8] && board[0] == enemyMark){
+      //Check for Diagonal wins
+      return 10;
+    }else if(board[2] == board[4] && board[2] == board[6] && board[2] == enemyMark){
+      return 10;
+    }else{
+      return "None";
+    }
+
+    }
     
     return {processChoices};
 
